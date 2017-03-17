2615fbd0ede615fcb3a39d69471deb4fd1dc447b
AndrianusTinas <andrianus_tinas.siskom@student.untan.ac.id>
2017-03-17 23:49:24 +0700

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Tinas Disini
                 </div>
 
                 <div class="links">
